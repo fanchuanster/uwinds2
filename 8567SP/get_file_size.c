1da3af06fbd892bb85b35d33656a2da5fe3d3cb9
Wen Dong <wen.dong@microfocus.com>
2021-01-24 16:18:33 +0800

    test

---
+++ 8567SP/get_file_size.c
@@ -0,0 +1,33 @@
+#include <unistd.h> 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]){ 
+
+ int fd1;
+ long filesize;
+
+
+ if ((fd1 = open(argv[1], O_RDONLY)) == -1) {
+
+   perror("file problem ");  
+
+   exit(1);
+ }
+
+
+ filesize = lseek(fd1, 0, SEEK_END);
+
+// in Case of an error exit from the loop  
+ if(filesize == -1){
+  perror("Reading problem ");  
+ }
+
+ close(fd1);
+ 
+ printf("%d\n", read_bytes);
+
+ exit(0);
+}
