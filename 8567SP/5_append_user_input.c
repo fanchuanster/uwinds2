76dad14f0306b9210425eefbe3f624a3c2e54206
Wen Dong <wen.dong@microfocus.com>
2021-01-24 17:49:09 +0800

    test

---
+++ 8567SP/5_append_user_input.c
@@ -0,0 +1,45 @@
+#include <unistd.h> 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]){ 
+
+ char buffer[100], append[256]; long int n1;
+
+ int fd1;
+
+
+ if((fd1 = open(argv[1], O_WRONLY)) == -1){
+
+   perror("file open problem ");
+
+   exit(1);
+ }
+
+
+ while((n1=read(fd1, buffer, 100)) > 0) {
+   printf("%s", buffer);
+ }
+ printf("\n+============please input the you would like to append, 256 chars at maximum================\n");
+ n1 = scanf_s("%s", append);
+ if (n1 > 0) {
+	 write(fd1, append, n1);
+ }
+
+
+// in Case of an error exit from the loop  
+
+ if(n1 == -1){
+
+  perror("Reading problem ");  
+
+  exit(2);
+
+ }
+
+ close(fd1);
+
+ exit(0);
+}
