2e2d6d1644f5ed178d89d23abd17c4c7dd43dba8
Wen Dong <wen.dong@microfocus.com>
2021-01-24 17:04:14 +0800

    test

--- 8567SP/3_create_file.c
+++ 8567SP/3_create_file.c
@@ -9,13 +9,16 @@ int main(int argc, char *argv[])
 	char* pp[2][4] = { { "101", "GM", "Buick", "2010" }, { "102", "Ford", "Lincoln", "2005" } };
 	int i, j, n1;
 	int fd1;
+	
 	if ((fd1 = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0700)) == -1) {
 		perror("file problem ");
 		exit(1);
 	}
 
-	for (i = 0; i < sizeof(pp) / sizeof(char**); i++) {
-		for (j = 0; j < sizeof(*pp) / sizeof(char*); j++) {
+	// for (i = 0; i < sizeof(pp) / sizeof(char**); i++) {
+		// for (j = 0; j < sizeof(*pp) / sizeof(char*); j++) {
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 4; j++) {
 			printf("%s", pp[i][j]);
 			n1 = write(fd1, pp[i][j], strlen(pp[i][j]));
 			if (n1 != strlen(pp[i][j])) {
