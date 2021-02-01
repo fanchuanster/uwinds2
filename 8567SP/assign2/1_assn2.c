4eb1fe4de5e5af44a99c8dd2100b9835e6ec4416
Wen Dong <wen.dong@microfocus.com>
2021-02-01 18:48:14 +0800

    test

--- 8567SP/assign2/1_assn2.c
+++ 8567SP/assign2/1_assn2.c
@@ -1,29 +1,41 @@
-#include<stdio.h> 
+#include <stdio.h>
+#include <unistd.h> 
+#include <fcntl.h>
 
-int main(int argc, char* argv[]) { 
-FILE *fp; 
-void filecopy(FILE *, FILE *); 
+int main(int argc, char *argv[])
+{
+    int fd;
+    void filecopy(int, int);
 
-if (argc == 1) { 
-filecopy(stdin, stdout); 
+    if (argc == 1)
+    {
+        filecopy(stdin, stdout);
+    }
+    else
+    {
+        while (--argc > 0)
+        {
+            if ((fd = open(*++argv, O_RDONLY)) == -1)
+            {
+                printf("cat: can not open %s\n", *argv);
+                return 1;
+            }
+            else
+            {
+                filecopy(fp, stdout);
+                fclose(fp);
+            }
+        }
+    }
+    return 0;
 }
-else { 
-while(--argc >0){ 
-if ((fp = fopen(*++argv, "r")) == NULL){ 
-printf("cat: can not open %s\n", *argv); 
-return 1; 
-}
-else { 
-filecopy(fp, stdout); 
-fclose(fp); 
-}	 
-} 
-}
-return 0; 
-}
-void filecopy(FILE *ifp, FILE *ofp) { 
-int c; 
-while ((c = getc(ifp)) != EOF) { 
-putc(c, ofp); 
-} 
+void filecopy(int fd1, int fd2)
+{
+    const int buffer_size = 100;
+    char buffer[buffer_size]
+    long read_size;
+    while ((read_size = read(fd1, buffer, buffer_size)) != -1)
+    {
+        write(fd2, buffer, read_size);
+    }
 }
