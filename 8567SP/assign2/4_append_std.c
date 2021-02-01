24f67fb6396bd90bae34d23e072bcbadb8ac465d
Wen Dong <wen.dong@microfocus.com>
2021-02-01 22:58:36 +0800

    test

---
+++ 8567SP/assign2/4_append_std.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    FILE* fd;
+    FILE* fdout;
+    int i;
+    void filecopy(int, int);
+
+    // for (i=0; i<argc; i++)
+    // {
+    //     printf("%s\n", argv[i]);
+    // }
+
+    if (argc < 3)
+    {
+        printf("usage: ./append filename1 filename2 combinednewfile");
+        return 1;
+    }
+
+    if ((fdout = fopen(argv[argc-1], "w+")) == -1)
+    {
+        printf("Failed to open file %s for writing", argv[argc-1]);
+        perror("error: ");
+        return (2);
+    }
+    
+    int paramscounter = argc;
+    while (--paramscounter > 1)
+    {
+        if ((fd = fopen(*++argv, "r")) == -1)
+        {
+            printf("cat: can not open %s\n", *argv);
+            return 3;
+        }
+        else
+        {
+            filecopy(fd, fdout);
+            fclose(fd);
+        }
+    }
+
+    lseek(fdout, 0, SEEK_SET);
+
+    filecopy(fdout, stdout);
+    fclose(fdout);
+
+    printf("\n");
+    return 0;
+}
+void filecopy(FILE* fd1, FILE* fd2)
+{
+    const int buffer_size = 100;
+    char buffer[buffer_size];
+    long read_size;
+    while ((read_size = fread(buffer, buffer_size, 1, fd1)) > 0)
+    {
+        fwrite(buffer, read_size, 1, fd2);
+    }
+}
