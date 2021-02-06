b875dfd4ba7979a2d066453bf10c18efff83c973
Wen Dong <wen.dong@microfocus.com>
2021-02-06 20:23:24 +0800

    est

--- 8567SP/assign3/wdq1.c
+++ 8567SP/assign3/wdq1.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 int main(){
-	printf("before fork, my pid is %d\n" , getpid());  
+	printf("before fork, my pid is %d, ppid is %d\n" , getpid(), getppid());
 	fork();
 	printf("after 1 fork, my pid is %d, ppid is %d\n" , getpid(), getppid());
 	fork();
 	printf("after 2 fork, my pid is %d, ppid is %d\n" , getpid(), getppid());
 	fork();
 	printf("after 3 fork, my pid is %d, ppid is %d\n" , getpid(), getppid());
-	printf("done, my pid is %d\n", getpid());
+	printf("done, my pid is %d, ppid is %d\n" , getpid(), getppid());
 }
