a0e2cedc93790a46440f4d4b62cb68b4196b2da2
Wen Dong <wen.dong@microfocus.com>
2021-02-07 17:52:25 +0800

    tst

---
+++ 8567SP/assign3/wdq5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {  
+	int newpid;
+	
+	const char* message_pattern = "I am a child process, my parent pid = %d, my pid = %d\n";
+
+	printf("I am the parent process, my pid = %d\n", getpid());
+
+	if ((newpid = fork()) == -1 )
+	{
+		perror("fork"); 
+		return -1;
+	}
+
+	if (newpid == 0) {
+		printf(message_pattern, getppid(), getpid());
+		newpid = fork();
+		if (newpid == 0) {
+			sleep(1);
+			printf(message_pattern, getppid(), getpid());
+		}
+	}
+	else {
+		newpid = fork();
+		
+		if (newpid == 0) {
+			printf(message_pattern, getppid(), getpid());
+			newpid = fork();
+			if (newpid == 0) {
+				sleep(1);
+				printf(message_pattern, getppid(), getpid());
+			}
+		} else {
+			printf("I am the parent process, my pid = %d\n", getpid());
+			sleep(2);
+		}
+	}
+}
+
+
