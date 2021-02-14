2492c1f995cd5a96f6018678296e1610b2770acf
Wen Dong <wen.dong@microfocus.com>
2021-02-14 19:22:33 +0800

    etst

--- 8567SP/assign1/3_writer.c
+++ 8567SP/assign1/3_writer.c
@@ -7,22 +7,38 @@
 int main(int argc, char *argv[])
 {
 	int fd1;
-	long n1;
-	char* str = "101   GM\tBuick\t2010\n102   Ford\tLincoln\t2005";
+	pid_t pid;
 	
-	if ((fd1 = open("list1.txt", O_CREAT | O_WRONLY | O_TRUNC, 0700)) == -1) {
+	if ((fd1 = open("wdq3.txt", O_CREAT | O_WRONLY | O_TRUNC, 0700)) == -1) {
 		perror("file problem ");
 		exit(1);
 	}
 
-	n1 = write(fd1, str, strlen(str));
-	if (n1 != strlen(str)) {
-		perror("write problem ");
-		exit(1);
-	}
-	close(fd1);
+	pid = fork();
 
-	printf("file list1.txt created", argv[1]);
+	if (pid == 0) {
+		write(fd1, "Hello from child process, and closing fd1");
+
+		/* sleep to allow parent process to say hello. */
+		sleep(1);
+		close(fd1);
+
+		/* prolong child process longevity to preclude impact by process exit which would impact 
+		 *  fd1 vadility
+		 */
+		sleep(2);
+	} else {
+		write(fd1, "Hello from parent process");
+
+		/* sleep for a while to ensure fd1 closed. */
+		sleep(2);
+		if (-1 == write(fd1, "Greetings from parent process")) {
+			perror("greetings failed ");
+		}
+		else {
+			close(fd1);
+		}
+	}
 	
     return 0;
 }
