823ce496fdb57267d68f5d8f5ada99cc5b070c45
Wen Dong <wen.dong@microfocus.com>
2021-02-15 20:14:18 +0800

    t

--- 8567SP/assign4/wdq5.c
+++ 8567SP/assign4/wdq5.c
@@ -6,114 +6,35 @@
 #include <assert.h>
 #include <sys/wait.h>
 
-static char tmpbuf[1000];
+void printSubCommand(char* command, char* optionalArgument){
+	const int MAXBUF = 256;
+	char buf[MAXBUF];
 
-#define	E_WRONG_STATEMENT	50
-#define	E_DIVISION_BY_ZERO	100
-#define	E_WRONG_OP			200
-
-const char VALID_OPERATORS[] = {'+', '-', '*', '/'};
-
-void printl(char* message) {
-	write(STDOUT_FILENO, message, strlen(message));
-	if (message[strlen(message)-1] != '\n') {
-		write(STDOUT_FILENO, "\n", 1);
-	}
-	fsync(STDOUT_FILENO);
+	sprintf(buf, "in sub process %d, executing %s %s\n", getpid(), command, optionalArgument);
+	printf(buf);
 }
 
-int arrayContains(const char* array, char c) {
-	int i =0;
-	for (i=0; i<sizeof(array)/sizeof(array[0]); i++) {
-		if (array[i] == c) {
-			return 1;
-		}
-	}
-	return 0;
-}
+void executeCommand(char* command, char* optionalArgument) {
+	pid_t childPid;
 
-int basicArithmeticsOperation(int operand1, char operator, int operand2) {
-	switch (operator) {
-		case '+':
-			return operand1 + operand2;
-		case '-':
-			return operand1 - operand2;
-		case '*':
-			return operand1 * operand2;
-		case '/':
-			return operand1 / operand2;
-		default:
-			assert(0);
-			break;
+	childPid = fork();
+	if (childPid == 0) {
+		printSubCommand(command, optionalArgument);
+		execlp(command, optionalArgument);
 	}
-}
-
-void childFunction(char* line) {
-	printl("I am a child working for my parent");
-	int operand1, operand2;
-	char operator;
-	int count = sscanf(line, "%d %c %d", &operand1, &operator, &operand2);
-	if (count != 3) {
-		exit(E_WRONG_STATEMENT);
-	}
-	else if (!arrayContains(VALID_OPERATORS, operator)) {
-		exit(E_WRONG_OP);
-	} else if (operand2 == 0 && operator == '/') {
-		exit(E_DIVISION_BY_ZERO);
-	}
-
-	sprintf(tmpbuf, "%d %c %d = %d", operand1, operator, operand2, basicArithmeticsOperation(operand1, operator, operand2));
-	printl(tmpbuf);
-
-	exit(0);
 }
 
 int main(int argc, char *argv[])
 {
 	const int MAXBUF = 256;
+	char buf[MAXBUF];
 
-	char inpbuf[MAXBUF];
-	pid_t pidc;
-
-	while(1) {
-		
-		printl("Enter an arithmetic statement, e.g., 34 + 132 >");
-
-		ssize_t readSize;
-		if (EOF == (readSize = read(STDIN_FILENO, inpbuf, MAXBUF))) {
-			printl("EOF read");
-			break;
-		}
-
-		pidc = fork();
-		if (pidc == 0) {
-			childFunction(inpbuf);
-		}
-		else {
-			printl("Created a child to make your operation, waiting");
-			
-			int status;
-			wait(&status);
-
-			status = WEXITSTATUS(status);
-
-			switch(status) {
-				case E_WRONG_STATEMENT:
-					printl("Wrong statement");
-					break;
-				case E_DIVISION_BY_ZERO:
-					printl("Division by zero");
-					break;
-				case E_WRONG_OP:
-					printl("Wrong operator");
-					break;
-				default:
-					assert(status == 0);
-					break;
-			}
-		}		
+	int i;
+	for (i=0; i<argc; i++) {
+		printf(argv[i]);
+		printf("\n");
 	}
-	
+		
     return 0;
 }
 
