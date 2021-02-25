bf9682caa042f7155d91d284dfec7518a78714c2
Wen Dong <wen.dong@microfocus.com>
2021-02-25 21:45:34 +0800

    t

---
+++ 8567SP/midterm/wdq1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include <unistd.h>
+
+int main()
+{
+    FILE *fp;
+    int fd2;
+    char name[50];
+    int roll_no, chars;
+    float marks;
+
+    const char* infilename = "records.txt";
+    const char* outfilename = "results.txt";
+
+    const char* resultpattern = "The result of multiplying %f by 2 is %f";
+
+    fp = fopen(infilename, "r");
+    if(fp == NULL)
+    {
+        perror("Error opening file\n");
+        exit(1);
+    }
+
+    if ((fd2 = open(outfilename, O_CREAT | O_RDWR, 700)) == -1)
+    {
+        perror("Error opening file for output");
+        exit(2);
+    }
+
+
+    while( fscanf(fp, "%f", &marks) != EOF )
+    {
+        printf("%.2f\n", marks);
+    }
+
+    fclose(fp);
+    close(fd2);
+    return 0;
+}
+
