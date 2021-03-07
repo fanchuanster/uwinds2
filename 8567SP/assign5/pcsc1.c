657484ace137062cea09f7b1b2480ebb48392b70
Wen Dong <wen.dong@microfocus.com>
2021-03-07 18:35:47 +0800

    t

--- 8567SP/assign5/pcsc1.c
+++ 8567SP/assign5/pcsc1.c
@@ -17,8 +17,7 @@ int main(int argc, char *argv[]){
 		while(1){
 			printf("Parent is running\n");
 			kill(pid, SIGUSR1);  
-			signal(SIGUSR1, action);  
-			printf("Parent paused\n");
+			signal(SIGUSR1, action);
 			pause();
 		}
 	}
@@ -26,11 +25,11 @@ int main(int argc, char *argv[]){
 		while(1){//child  
 			signal(SIGUSR1, action);  
 			kill(getppid(), SIGUSR1);
-			sleep(1);
-			int status;
-			waitpid(getppid(), &status, 0);
-			printf("parent has terminated with status %d\n", WEXITSTATUS(status));
-			printf("child paused\n");
+			// sleep(1);
+			// int status;
+			// waitpid(getppid(), &status, 0);
+			// printf("parent has terminated with status %d\n", WEXITSTATUS(status));
+			// printf("child paused\n");
 			pause();
 			printf("Child is running\n");  
 			
