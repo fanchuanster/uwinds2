a1e56387753302d91b4d0ce6643728c96a5d8e14
Wen Dong <wen.dong@microfocus.com>
2021-03-07 16:17:38 +0800

    t

---
+++ 8567SP/assign5/wdq2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+void childDeath(int dummy){  
+	int status;
+	pid_t pid = wait(&status);
+	printf("Child %d has terminated with status %d\n", pid, WEXITSTATUS(status));
+}
+
+int main(int argc, char *argv[]){  
+	pid_t pid;
+
+    // install handler for SIGCHLD
+	signal(SIGCHLD, childDeath);
+
+    if((pid=fork())==0){ //child process code
+        sleep(5);  
+        exit(10);
+    }
+    printf("Created a child, pid=%d, pause...\n", pid);
+    pause();
+    printf("parent process ends.")
+}
