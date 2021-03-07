85eb7ec017e4960cf7142d467345c1a51cf2d254
Wen Dong <wen.dong@microfocus.com>
2021-03-07 18:09:29 +0800

    t

---
+++ 8567SP/assign5/wdq4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+void my_alarm_handler(int dummy){  
+	printf("Ding!\n", getpid(), dummy);
+}
+
+int main(int argc, char *argv[]){  
+	pid_t pid;
+    void (*oldhandler)(int);
+    int alarm_in_seconds;
+
+    if (argc != 2) {
+        printf("usage: alarm n\n");
+        exit(1);
+    }
+
+    printf("alarm application starting\n");
+
+    alarm_in_seconds = atoi(argv[1]);
+
+	oldhandler = signal(SIGALRM, my_alarm_handler);
+
+    if((pid=fork())==0){ //child process code
+        sleep(alarm_in_seconds);
+        kill(getppid(), SIGALRM);
+    }
+    printf("waiting for alarm to go off\n");
+    pause();
+
+    // restore handler.
+    oldhandler = signal(SIGALRM, oldhandler);
+
+    printf("done");
+
+    return 0;
+}
