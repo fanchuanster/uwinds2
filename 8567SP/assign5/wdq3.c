fa39a710331c7813dc2f047d934315f536a1446b
Wen Dong <wen.dong@microfocus.com>
2021-03-07 16:52:05 +0800

    t

--- 8567SP/assign5/wdq3.c
+++ 8567SP/assign5/wdq3.c
@@ -11,23 +11,32 @@ void myhandler(int dummy){
 int main(int argc, char *argv[]){  
 	pid_t pid;
 
+    void (*oldhandler)();
+    void (*oldhandler2)();
+
     // install handler for Ctrl + C
-	signal(SIGINT, myhandler);
+	oldhandler = signal(SIGINT, myhandler);
 
     // ignore Ctrl + Z 24
-    signal(SIGSTOP, SIG_IGN);
+    oldhandler2 = signal(SIGSTOP, SIG_IGN);
 
+    printf("for ignored Ctrl + Z or Ctrl + C, test them within 10 seconds.\n")
     int i;
-    while (i++ < 10) {
+    while (i++ <= 10) {
         printf("w %d\n", i);
         sleep(1);
     }
 
-    // if((pid=fork())==0){ //child process code
-    //     sleep(5);  
-    //     exit(10);
-    // }
-    // printf("Created a child, pid=%d, pause...\n", pid);
-    // pause();
-    // printf("parent process ended.\n");
+    if((pid=fork())==0){ //child process code
+        execlp("./donothing", (char*)NULL);
+    }
+    
+    for(i=1; i<=15; i++){
+    printf("I am in parent process.\n");
+    //send a signal to child
+    sleep(1);}
+
+    // restore handlers
+    signal(SIGINT, oldhandler);
+    signal(SIGSTOP, oldhandler2);
 }
