8cbd63fb24a1f5c4dc176a5470d84e1c5ea57eac
Wen Dong <wen.dong@microfocus.com>
2021-03-07 16:06:47 +0800

    t

--- 8567SP/assign5/wdq1.c
+++ 8567SP/assign5/wdq1.c
@@ -5,24 +5,14 @@
 
 int main(int argc, char *argv[]) {
 	const int max = 1000;
+	int result;
 	int i = 0;
 	char* desc = 0;
 	while (i++ < max) {
 		desc = strsignal(i);
-		if (strcmp("Unknown signal", desc) != 0) {
+		if (strstr(desc, "Unknown signal") != 0) {
 			printf("%d - %s\n", i, desc);
 		}		
 	}
-	char str1[] = "abcd", str2[] = "abCd", str3[] = "abcd";
-    int result;
-
-    // comparing strings str1 and str2
-    result = strcmp(str1, str2);
-    printf("strcmp(str1, str2) = %d\n", result);
-
-    // comparing strings str1 and str3
-    result = strcmp(str1, str3);
-    printf("strcmp(str1, str3) = %d\n", result);
-
-    return 0;
+	return 0;
 }
