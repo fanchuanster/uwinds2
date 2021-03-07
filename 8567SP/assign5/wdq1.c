9ee0bfac9d2e82fd9f24bb814e22b8d8cb9f3236
Wen Dong <wen.dong@microfocus.com>
2021-03-07 16:08:28 +0800

    t

--- 8567SP/assign5/wdq1.c
+++ 8567SP/assign5/wdq1.c
@@ -5,14 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	const int max = 1000;
-	int result;
+	int counter;
 	int i = 0;
 	char* desc = 0;
 	while (i++ < max) {
 		desc = strsignal(i);
 		if (strstr(desc, "Unknown signal") == 0) {
+			counter++;
 			printf("%d - %s\n", i, desc);
 		}		
 	}
+
+	printf("in all, %d signals are supported by the system.\n", counter);
 	return 0;
 }
